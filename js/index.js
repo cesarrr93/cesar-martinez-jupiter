ca56c919ef7d3d822a07afff4b440a17a1f24b3c
Cesar Martinez <cesar.martinez.perez.415@gmail.com>
2025-01-16 13:18:30 -0800

    revisions for lesson 10

--- js/index.js
+++ js/index.js
@@ -7,16 +7,15 @@ const body = document.querySelector("body");
 // Adding footer to body
 body.appendChild(footer);
 
-// Create date 
+// Create date
 const today = new Date();
 // Storing Current Year
 const thisYear = today.getFullYear();
 
-
 // Adding Copyright and year to footer
 const copyright = document.createElement("p");
 // adding text to p tag
-copyright.textContent = ` &copy; Cesar Martinez ${thisYear}`
+copyright.textContent =`© Cesar Martinez ${thisYear}`;
 
 // adding copyright to footer
 footer.appendChild(copyright);
